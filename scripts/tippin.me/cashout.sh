ca7a63172397854d2bfab9af672289f276a59111
Melvin Carvalho <melvincarvalho@gmail.com>
2019-01-13 07:41:54 +0100

    tipping.me cashout

---
+++ scripts/tippin.me/cashout.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+CASHOUT_URI="https://tippin.me/lndreq/cachout.php"
+
+usage() {
+  echo "Set SESSIONID to run"
+  exit -1
+}
+
+if [ -z "$SESSIONID" ]
+then
+  usage
+else
+  BALANCE=$(2>/dev/null curl -X POST -H 'Accept: application/json, text/javascript, */*; q=0.01' "$CASHOUT_URI" -H "Cookie: PHPSESSID=$SESSIONID" --data "payreq=$PAYREQ" | sed 's/.*"\(ln[^"]*\)".*/\1/')
+  echo $BALANCE
+fi
+
+
