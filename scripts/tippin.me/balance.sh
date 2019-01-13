3bb4d3bf35728ff08dc9161984ee9c049820cabd
Melvin Carvalho <melvincarvalho@gmail.com>
2019-01-14 00:18:05 +0100

    Add readme

--- scripts/tippin.me/balance.sh
+++ scripts/tippin.me/balance.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# A script to get your tippin.me balance from the command line
+# In order to run you must set PHPSESSID which can be found in
+# your cookie in your tippin.me session
+
 set -e
 
 BALANCE_URI="https://tippin.me/dashboard.php"
 
 usage() {
-  echo "Set the PHPSESSID env variable.  It can be found in your cookie as PHPSESSID"
+  echo "Set the PHPSESSID env variable.  It can be found in your tippin.me cookie." 1>&2
   exit -1
 }
 
