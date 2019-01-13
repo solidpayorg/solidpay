acaa56397bc768a1e301cab7a6c145e6f9cc7984
Melvin Carvalho <melvincarvalho@gmail.com>
2019-01-14 00:14:26 +0100

    cleanup session id

--- scripts/tippin.me/balance.sh
+++ scripts/tippin.me/balance.sh
@@ -5,15 +5,14 @@ set -e
 BALANCE_URI="https://tippin.me/dashboard.php"
 
 usage() {
-  echo "Set SESSIONID environment variable to run"
+  echo "Set the PHPSESSID env variable.  It can be found in your cookie as PHPSESSID"
   exit -1
 }
 
-if [ -z "$SESSIONID" ]
-then
+if [ -z "$PHPSESSID" ] ; then
   usage
 else
-  BALANCE=$(2>/dev/null curl "$BALANCE_URI" -H "Cookie: PHPSESSID=$SESSIONID" | grep 'userBalance =' | tr -dc '0-9')
+  BALANCE=$(2>/dev/null curl "$BALANCE_URI" -H "Cookie: PHPSESSID=$PHPSESSID" | grep 'userBalance =' | tr -dc '0-9')
   echo $BALANCE
 fi
 
