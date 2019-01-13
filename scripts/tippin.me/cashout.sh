2d82d7ea1eeb2bac5eaff49b123b799c75b7dc6f
Melvin Carvalho <melvincarvalho@gmail.com>
2019-01-13 07:42:33 +0100

    remove sed

--- scripts/tippin.me/cashout.sh
+++ scripts/tippin.me/cashout.sh
@@ -11,7 +11,7 @@ if [ -z "$SESSIONID" ]
 then
   usage
 else
-  BALANCE=$(2>/dev/null curl -X POST -H 'Accept: application/json, text/javascript, */*; q=0.01' "$CASHOUT_URI" -H "Cookie: PHPSESSID=$SESSIONID" --data "payreq=$PAYREQ" | sed 's/.*"\(ln[^"]*\)".*/\1/')
+  BALANCE=$(2>/dev/null curl -X POST -H 'Accept: application/json, text/javascript, */*; q=0.01' "$CASHOUT_URI" -H "Cookie: PHPSESSID=$SESSIONID" --data "payreq=$PAYREQ")
   echo $BALANCE
 fi
 
