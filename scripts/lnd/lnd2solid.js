551ab937eb0bfe5c4bdfb71510366ea18f5d03ce
Melvin Carvalho <melvincarvalho@gmail.com>
2019-01-27 18:32:22 +0100

    fix addr

--- scripts/lnd/lnd2solid.js
+++ scripts/lnd/lnd2solid.js
@@ -1,11 +1,12 @@
 var ln = require("./ln.json")
 
 ln.nodes.forEach((k) => { 
+    var addr = k.addresses.map(a => a.addr)
     var turtle = `@prefix : <https://ln.w3id.org/> .
     
 <#this> a :Node ;
   :pub_key "${k.pub_key}" ;
-  :addr """${k.addresses.map(a => a.length)}""";
+  :addr """${addr}""";
   :alias """${k.alias}""" ;
   :color "${k.color}" ;
   :last_update "${k.last_update}" .`
